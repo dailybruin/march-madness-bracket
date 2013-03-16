1795bb3b3e3b475d1a7c7e8c3782ba36fb9aaf59
Daniel Duan <danielduan88@yahoo.com>
2013-03-16 12:53:02 -0700

    javascript onclicks all assigned

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,12 @@ function squareToWide(a) {
     return a.replace("square","wide");
 }
 
-function updateDropdown(img1, id1, img2, id2) {
-	var menu1 = document.getElementById(img1).src;
-	var menu2 = document.getElementById(img2).src;
+function getNameFromURL(img) {
+	var imgurl = document.getElementById(img).src;
+	return imgurl.substring(imgurl.lastIndexOf("/") + 1, imgurl.lastIndexOf("square"));
+}
+
+function getInnerImage() {
+	return this.getElementsByTagName("img")[0].src;
 	
-	menu1 = menu1.substring(menu1.lastIndexOf("/") + 1, menu1.lastIndexOf("square"));
-	menu2 = menu2.substring(menu2.lastIndexOf("/") + 1, menu2.lastIndexOf("square"));
-	
-	document.getElementById(id1).innerHTML = menu1;
-	document.getElementById(id2).innerHTML = menu2;
 }
