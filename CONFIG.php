2e14af09b3471b353e9358732bb904280405699d
Byron Lutz <byronlutz@gmail.com>
2013-03-18 02:53:46 -0700

    Changed settings of config file

--- CONFIG.php
+++ CONFIG.php
@@ -9,4 +9,8 @@ define('DB_USER', 'mmadness');
 define('DB_PASSWORD', 'mmadness');
 define('DB_HOST', 'localhost'); // You probably don't need to change this
 
+// Facebook info
+define('FB_APPID', '');
+define('FB_SECRET', '');
+
 ?>
